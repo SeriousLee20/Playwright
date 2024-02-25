bf50169667c0e2f63b8ade599a3c7415f23a59df
SeriousLee20 <LHuiXin01@gmail.com>
2024-02-25 18:14:08 +0800

    main:playwright with typescript

---
+++ tests/typescript/typescript.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect, selectors } from "@playwright/test";
+
+const SELECTORS = {
+  firstName: "#firstName",
+  age: "#age",
+  isStudent: "#isStudent",
+  apply: "#applyData",
+  displayFirstName: "#displayFirstName",
+  displayAge: "#displayAge",
+  displayIsStudent: "#displayIsStudent",
+};
+
+test.describe("Variable declarations & types", () => {
+  let firstName: string = "John";
+  let age: number = 30;
+  let isStudent: boolean = false;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(
+      "file:/Users/amber/Documents/Testing/Playwright/tests/typescript/index.html"
+    );
+  });
+
+  test("Declarations & types", async ({ page }) => {
+    await page.fill(SELECTORS.firstName, firstName);
+    await page.fill(SELECTORS.age, age.toString());
+    if (isStudent) {
+      await page.check(SELECTORS.isStudent);
+    }
+    await page.click(SELECTORS.apply);
+
+    expect(await page.textContent(SELECTORS.displayFirstName)).toBe(
+      `${firstName}`
+    );
+    expect(await page.textContent(SELECTORS.displayAge)).toBe(
+      `${age.toString()}`
+    );
+    expect(await page.textContent(SELECTORS.displayIsStudent)).toBe(
+      `${isStudent ? "Yes" : "No"}`
+    );
+    expect(await page.isChecked(SELECTORS.isStudent));
+  });
+});
+
+test.describe("Type Definitions and Interfaces", () => {
+  test.beforeEach(async({ page }) => {
+    await page.goto(
+      "file:/Users/amber/Documents/Testing/Playwright/tests/typescript/index.html"
+    );
+  });
+
+  test("Type Def and Interfaces", async ({ page }) => {
+    type User = {
+      firstName: string;
+      age: number;
+      isStudent: boolean;
+    };
+
+    let user: User = {
+      firstName: "Jane",
+      age: 30,
+      isStudent: false,
+    };
+
+    await page.fill(SELECTORS.firstName, user.firstName);
+    await page.fill(SELECTORS.age, user.age.toString());
+    if (user.isStudent) {
+      await page.check(SELECTORS.isStudent);
+    }
+
+    await page.click(SELECTORS.apply);
+
+    expect(await page.textContent(SELECTORS.displayFirstName)).toBe(user.firstName);
+    expect(await page.textContent(SELECTORS.displayAge)).toBe(user.age.toString());
+    expect(await page.textContent(SELECTORS.displayIsStudent)).toBe(
+      user.isStudent ? "Yes" : "No"
+    );
+  });
+});
